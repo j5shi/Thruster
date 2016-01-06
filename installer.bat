ab8742b393c2c233adbc3c3461001415b4d1d25f
j5shi.vip@gmail.com <j5shi.vip@gmail.com>
2016-01-06 23:21:20 +0800

    add python dependency to solve the incompatibility issues.

--- installer.bat
+++ installer.bat
@@ -2,10 +2,8 @@
 
 taskkill /IM Launchy.exe /F
 
-xcopy /Y boost_python-vc80-mt-1_41.dll "%programfiles(x86)%\Launchy"
-xcopy /Y python27.dll                  "%programfiles(x86)%\Launchy"
-
-xcopy /E /Y plugins "%programfiles(x86)%\Launchy\plugins"
+xcopy /E /Y pylibs                        "%programfiles(x86)%\Launchy" 
+xcopy /E /Y plugins                       "%programfiles(x86)%\Launchy\plugins"
 
 echo **************************************************
 echo * Thruster has been installed successfully,      * 
